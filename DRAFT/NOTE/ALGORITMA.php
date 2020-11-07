9e38911107d6265a638fc661ef99374be08c8452
sukipri <sukipri9985@gmail.com>
2020-11-07 08:04:35 +0700

    Update ALGORITMA.php

--- DRAFT/NOTE/ALGORITMA.php
+++ DRAFT/NOTE/ALGORITMA.php
@@ -7,8 +7,19 @@
 
 	<?php 
 		/*Algoritma & multifungsi Menu Kode data */
-					
-
+			1. Entri Form Prosedure
+				[
+				  1.1 generate kode dari sistem
+				  1.2 entri penamaan kode
+				]
+			2. Proses simpan()
+				
+			/*   
+			  pemufungsian kode data , untuk menjadi master atau penggolongan(flag) saat melakukan
+			   pengentrian / proses pemasukan data baru
+			
+			*/
+		
 
 			
 			
